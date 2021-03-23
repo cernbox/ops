b01fc973257ecb4c060c10de44aeff6c6e4f62ae
Sami Chebbi <sami.mohamed.chebbi@cern.ch>
2021-03-23 16:38:53 +0100

    Pushing into qa

--- cernbox-grid.sh
+++ cernbox-grid.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 rqf=$1
 username=$2
 dn=$3
 
+
 now=$(date +'%s')
 qa_branch="home-${rqf}-grid-${username}-${now}"
-#legacy_branch="legacy-${rqf}-grid-${username}-${now}"
 mapping="  \"${dn}\": ${username}"
 commit="${rqf} home: add user ${username} to gridmap file"
 
@@ -20,33 +22,19 @@ quiet_git() {
 	git "$@" &> /dev/null
 }
 
-tmp_dir=$(mktemp -d -t cernbox-grid-XXXX)
+#tmp_dir=$(mktemp -d -t cernbox-grid-XXXX)
+echo "Temporary directory: ${tmp_dir}"
 cd $tmp_dir
 quiet_git clone https://:@gitlab.cern.ch:8443/ai/it-puppet-hostgroup-eos.git
 cd it-puppet-hostgroup-eos
-quiet_git checkout qa
+#git checkout master
+quiet_git checkout -b ${qa_branch}
 echo "${mapping}" >> data/hostgroup/eos/home.yaml
 quiet_git add data/hostgroup/eos/home.yaml
 quiet_git commit -m "${commit}"
 cherry=$(git log --oneline | head -1 | awk '{print $1}')
-quiet_git push origin qa
-
-#quiet_git checkout legacy
-#quiet_git checkout -b ${legacy_branch} 
-#echo "${mapping}" >> data/hostgroup/eos/user.yaml
-#quiet_git add data/hostgroup/eos/user.yaml
-#quiet_git commit -m "${commit}"
-
-# push branches
-
-quiet_git checkout master
-quiet_git checkout -b ${qa_branch}
-quiet_git cherry-pick ${cherry}
+quiet_git push origin ${qa_branch}:qa
 master_link=$(git push origin ${qa_branch} 2>&1 | grep "merge_request")
-
-#quiet_git checkout ${legacy_branch}
-#legacy_link=$(git push origin ${legacy_branch} 2>&1 | grep "merge_request")
 
 # https://gitlab.cern.ch/ai/it-puppet-hostgroup-eos/merge_requests/new?merge_request%5Bsource_branch%5D=legacy-RQF1389645-grid-prrout-1566827042
 echo "Visit to create merge request: ${master_link}&merge_request%5Btarget_branch%5D=master"
-#echo "Visit to create merge request: ${legacy_link}&merge_request%5Btarget_branch%5D=legacy"
